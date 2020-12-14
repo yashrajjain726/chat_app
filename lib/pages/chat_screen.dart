a252774bfa1a3380349bce24b906cd0d0c8e26cb
Yashraj Jain <yashrajjain726@gmail.com>
2020-12-14 20:07:40 +0530

    Implemeted ListView Builder

---
+++ lib/pages/chat_screen.dart
@@ -0,0 +1,70 @@
+import 'package:chat_app/class/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _textComposerWidget() {
+    return IconTheme(
+      data: IconThemeData(color: Colors.pink[200]),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enter a Message"),
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send_rounded),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          ),
+        ),
+        Divider(height: 1.0),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        )
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,15 @@
+import 'package:chat_app/pages/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Chattly  ',
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          centerTitle: true,
+          title: Text('Syrun'),
         ),
-      ),
-    );
+        body: ChatScreen());
   }
 }
