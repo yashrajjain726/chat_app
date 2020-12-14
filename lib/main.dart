82efbdde7c5211da5a29496008b2d6dc0353f271
Yashraj Jain <yashrajjain726@gmail.com>
2020-12-14 14:27:37 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:chat_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Chat App",
+      home: HomePage(),
+    );
+  }
+}
